6b04617e82c0b8387545321e3ee22ee8edb6b339
ksinnovv-projects <admin@stylin.in>
2025-02-18 18:30:42 +0530

    add:added React and Structured Folders

---
+++ food-heritage-web/src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+ 
+
+  return (
+    <>
+     <h2 className="font-bold text-5xl flex flex-col h-screen justify-center items-center ">This is Official Webpage of Food & Heritage  </h2>
+    </>
+  )
+}
+
+export default App
